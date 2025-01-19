056fbf2595ae1cb26ea99f1459285ad0c79d1e46
Tom Dudley <git@tomdudley.io>
2025-01-19 12:06:49 +0000

    Move Pin out to separate component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,11 @@
 <script lang="ts">
-    import { getCurrentWindow } from "@tauri-apps/api/window";
+    import PinButton from "./PinButton.svelte";
     import TodoList from "./TodoList.svelte";
     import "@fortawesome/fontawesome-free/css/all.min.css";
-
-    let loading = $state(true);
-
-    let alwaysOnTop = $state(false);
-
-    async function toggleAlwaysOnTop() {
-        alwaysOnTop = !alwaysOnTop;
-        await getCurrentWindow().setAlwaysOnTop(alwaysOnTop);
-    }
 </script>
 
 <main class="container">
-    <div style="display: flex; justify-content: flex-end">
-        <button onclick={toggleAlwaysOnTop}>
-            <i class="fa fa-thumbtack"></i>
-            {#if alwaysOnTop}
-                <div>Enabled</div>
-            {:else}
-                <div>Disabled</div>
-            {/if}
-        </button>
-    </div>
+    <PinButton />
     <h1>Todo List</h1>
 
     <p>Enter some Todos!</p>
@@ -50,7 +32,7 @@
 
     .container {
         margin: 0;
-        padding-top: 10vh;
+        padding-top: 5vh;
         display: flex;
         flex-direction: column;
         justify-content: center;
@@ -65,40 +47,6 @@
         :root {
             color: #f6f6f6;
             background-color: #2f2f2f;
-        }
-    }
-    button {
-        border-radius: 8px;
-        border: 1px solid transparent;
-        padding: 0.6em 1.2em;
-        font-size: 1em;
-        font-weight: 500;
-        font-family: inherit;
-        color: #0f0f0f;
-        background-color: #ffffff;
-        transition: border-color 0.25s;
-        box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
-    }
-
-    button {
-        cursor: pointer;
-    }
-
-    button:hover {
-        border-color: #396cd8;
-    }
-    button:active {
-        border-color: #396cd8;
-        background-color: #e8e8e8;
-    }
-    @media (prefers-color-scheme: dark) {
-        input,
-        button {
-            color: #ffffff;
-            background-color: #0f0f0f98;
-        }
-        button:active {
-            background-color: #0f0f0f69;
         }
     }
 </style>
---
+++ src/routes/PinButton.svelte
@@ -0,0 +1,58 @@
+<script>
+    import { getCurrentWindow } from "@tauri-apps/api/window";
+
+    let alwaysOnTop = $state(false);
+
+    async function toggleAlwaysOnTop() {
+        alwaysOnTop = !alwaysOnTop;
+        await getCurrentWindow().setAlwaysOnTop(alwaysOnTop);
+    }
+</script>
+
+<div style="display: flex; justify-content: flex-end">
+    <button onclick={toggleAlwaysOnTop}>
+        <i class="fa fa-thumbtack"></i>
+        {#if alwaysOnTop}
+            <div>Enabled</div>
+        {:else}
+            <div>Disabled</div>
+        {/if}
+    </button>
+</div>
+
+<style>
+    button {
+        border-radius: 8px;
+        border: 1px solid transparent;
+        padding: 0.6em 1.2em;
+        font-size: 1em;
+        font-weight: 500;
+        font-family: inherit;
+        color: #0f0f0f;
+        background-color: #ffffff;
+        transition: border-color 0.25s;
+        box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    button:hover {
+        border-color: #396cd8;
+    }
+    button:active {
+        border-color: #396cd8;
+        background-color: #e8e8e8;
+    }
+    @media (prefers-color-scheme: dark) {
+        input,
+        button {
+            color: #ffffff;
+            background-color: #0f0f0f98;
+        }
+        button:active {
+            background-color: #0f0f0f69;
+        }
+    }
+</style>
